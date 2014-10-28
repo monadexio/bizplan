8c4151847ba52bba95e9d7ac1d0def91f97fdd7e
hongchao <hongchao@klarna.com>
2014-10-28 08:22:49 +0100

    remove everything except for README.md

--- src/final/AppUsingFinalClassV2.java
+++
@@ -1,9 +0,0 @@
-public class AppUsingFinalClassV2 {
-
-    public static void main(String[] args) throws Exception {
-        new ClassOverriding().redefineClass();
-
-        new FinalClass().callMe();
-    }
-
-}
